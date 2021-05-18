98f92389c966b1668ea9ea3507a4c8c9e9ccc0ed
necmigunduz <necmigunduz@gmail.com>
2021-05-18 17:28:14 +0300

    Setup 1st Part

---
+++ src/Scenes/GameScene.js
@@ -0,0 +1,17 @@
+import 'phaser';
+import logo from '../assets/logo_peacemaker.png';
+ 
+export default class GameScene extends Phaser.Scene {
+  constructor () {
+    super('Game');
+  }
+ 
+  preload () {
+    // load images
+    this.load.image('logo', logo);
+  }
+ 
+  create () {
+    this.add.image(400, 300, 'logo');
+  }
+};
