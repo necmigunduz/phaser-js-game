577849681b872413bccc3f3fbd1cf095d9602eb4
necmigunduz <necmigunduz@gmail.com>
2021-05-21 17:19:48 +0300

    Add colliders

--- src/Scenes/Entities.js
+++ src/Scenes/Entities.js
@@ -10,6 +10,39 @@ class Entity extends Phaser.GameObjects.Sprite {
     this.setData("type", type);
     this.setData("isDead", false);
   }
+  
+  explode = function(canDestroy){
+    if (!this.getData("isDead")) {
+      // Set the texture to the explosion image, then play the animation
+      this.setTexture("sion");  // this refers to the same animation key we used when we added this.anims.create previously
+      this.play("explosion"); // play the animation
+
+      // pick a random explosion sound within the array we defined in this.sfx in SceneMain
+      // this.scene.sfx.explosions[Phaser.Math.Between(0, this.scene.sfx.explosions.length - 1)].play();
+
+      if (this.shootTimer !== undefined) {
+        if (this.shootTimer) {
+          this.shootTimer.remove(false);
+        }
+      }
+
+      this.setAngle(0);
+      this.body.setVelocity(0, 0);
+
+      this.on('animationcomplete', function() {
+
+        if (canDestroy) {
+          this.destroy();
+        }
+        else {
+          this.setVisible(false);
+        }
+
+      }, this);
+
+      this.setData("isDead", true);
+    }
+  }
 }
 
 class Player extends Entity {
@@ -375,4 +408,4 @@ class PlayerLaser extends Entity {
   }
 }
 
-export { Player, ChaserShip, CarrierShip, GunShip, HollowShip };
+export { Player, ChaserShip, CarrierShip, GunShip, HollowShip, EnemyLaser, PlayerLaser };
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,5 +1,5 @@
 import 'phaser';
-import { Player, ChaserShip, CarrierShip, GunShip, HollowShip } from './Entities';
+import { Player, ChaserShip, CarrierShip, GunShip, HollowShip, EnemyLaser, PlayerLaser } from './Entities';
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -241,10 +241,64 @@ export default class GameScene extends Phaser.Scene {
       callbackScope: this,
       loop: true
     }); 
+
+    this.physics.add.collider(this.playerLasers, this.enemies, function(playerLaser, enemy) {
+      if (enemy) {
+        if (enemy.onDestroy !== undefined) {
+          enemy.onDestroy();
+        }
+      
+        enemy.explode(true);
+        playerLaser.destroy();
+      }
+    });
+
+    this.physics.add.collider(this.enemyLasers, this.player, function(enemyLaser, player) {
+      if (player) {
+        if (player.onDestroy !== undefined) {
+          player.onDestroy();
+        }
+      
+        player.explode(true);
+        enemyLaser.destroy();
+      }
+    });
+
+    this.physics.add.overlap(this.player, this.enemies, function(player, enemy) {
+      if (!player.getData("isDead") &&
+          !enemy.getData("isDead")) {
+        player.explode(false);
+        enemy.explode(true);
+      }
+    });
   }
 
   update() {
     this.player.update();
+
+    // if (!this.player.getData("isDead")) {
+    //   this.player.update();
+    //   if (this.keyW.isDown) {
+    //     this.player.moveUp();
+    //   }
+    //   else if (this.keyS.isDown) {
+    //     this.player.moveDown();
+    //   }
+    //   if (this.keyA.isDown) {
+    //     this.player.moveLeft();
+    //   }
+    //   else if (this.keyD.isDown) {
+    //     this.player.moveRight();
+    //   }
+    
+    //   if (this.keySpace.isDown) {
+    //     this.player.setData("isShooting", true);
+    //   }
+    //   else {
+    //     this.player.setData("timerShootTick", this.player.getData("timerShootDelay") - 1);
+    //     this.player.setData("isShooting", false);
+    //   }
+    // }
 
     if (this.keyW.isDown) {
       this.player.moveUp();
